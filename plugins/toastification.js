b96ec52cdee5125995f6d6428439dc6c9d58ee0e
Takayuki-Suda <takayuki.suda.job@gmail.com>
2025-01-06 11:41:17 +0900

    fix:"payload.addReplacer is not a function" エラーを修正するため、カスタムリデューサーを削除

--- plugins/toastification.js
+++ plugins/toastification.js
@@ -20,14 +20,4 @@ export default defineNuxtPlugin((nuxtApp) => {
 
   // トースト通知のプラグインをVueアプリに登録
   nuxtApp.vueApp.use(Toast, options);
-
-  // カスタムリデューサー/リバイバーを設定する
-  const { payload } = nuxtApp;
-  payload.addReplacer((key, value) => {
-    // 特定のキーや関数を除外する処理
-    if (typeof value === "function") {
-      return undefined; // 関数を除外
-    }
-    return value; // その他の値はそのまま
-  });
 });
